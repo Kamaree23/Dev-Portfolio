27452706dfdbbf916acb9b9b312460c1cd2d8157
Kamaree Robinson <kamareerobinson@gmail.com>
2023-01-21 15:12:10 -0500

    added some css

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from 'react'
 import './App.css'
+import { Link } from 'react-router-dom'
 
 export default function App() {
   return (
     <div className='grid'>
       <div className='num1'>
         <h5>
-          <pre> Travel API </pre>
+          <Link to=' '> Travel API </Link>
         </h5>
         <h6>
-          <pre> Here is a Travel Application that
-             I Engineered using CRUD and a REST API.</pre>
-        </h6>
+          <pre> Here is a Travel Application 
+            <br></br> that I Engineered using CRUD and 
+            <br></br> a REST API.</pre>
+        </h6> 
       </div>
       <div className='num2'>
         <h5>
-          <pre> Pokemon Game </pre>
+          <Link to=' '> Pokemon Game </Link>
         </h5>
         <h6>
-          <pre> Using Django I had a little fun creating a Pokemon game that lets you View catch and battle Pokemon </pre>
+          <pre> Using Django I created a <br></br> fullstack program to search, catch<br></br> and battle with pokemon</pre>
         </h6>
       </div>
       <div className='num3'>
         <h5>
-          <pre> ROI Calculator </pre>
+          <pre> 
+            <Link to=' '>ROI Calculator</Link>
+            </pre>
         </h5>
         <h6>
           <pre> Calculate Your ROI here! </pre>
